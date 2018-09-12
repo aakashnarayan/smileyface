477c3434bf23fbae185e55c5512b5db9c8bb0f39
aakashnarayan <42978492+aakashnarayan@users.noreply.github.com>
2018-09-12 14:07:36 -0400

    Add files via upload

---
+++ src/package1/Face.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Aakash Narayan
+ * Smiley Face
+ * Period 7
+ * 
+ */
+
+
+
+
+package package1;
+
+public class Face {
+
+	public static void main(String[] args) 
+	{	
+		System.out.print("  \\\\\\\\\\\\\\ \n  | * *  | \n  |  <   | \n  | \\__/ | \n   \\____/");
+	}
+
+}
